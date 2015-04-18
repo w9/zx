41d6563aa2439449a46375f65818014f548c5f4f
xzhu <zhuxun2@gmail.com>
2015-04-17 16:49:17 -1000

    afdaf

--- R/zx.R
+++ R/zx.R
@@ -17,6 +17,6 @@ pca <- function(mat, phe, logged=F) {
   ggdat <- data.frame(n=1:length(pr$x[,1]), x=pr$x[,1], y=pr$x[,2], x.t=jittered$x, y.t=jittered$y)
   ggplot(ggdat) +
     geom_point(aes(x=x, y=y)) +
-    geom_text(aes(x=x.t, y=y.t, label=1:nrow(ggdat)), alpha=0.2) +
+    geom_text(aes(x=x.t, y=y.t, label=n, alpha=0.2) +
     labs(x=pcLabel(1),y=pcLabel(2))
 }
