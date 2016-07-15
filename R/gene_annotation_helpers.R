9f57bb1bbbf584e164197089911f03456cca6cd0
Xun Zhu <zhuxun2@gmail.com>
2016-07-15 13:23:03 -1000

    commit

--- R/gene_annotation_helpers.R
+++ R/gene_annotation_helpers.R
@@ -8,7 +8,7 @@
 #' @import AnnotationDbi
 #' @import dplyr
 gene_annotation <-
-  function(genes_, organism_, format_='markdown', use_description_=F) {
+  function(genes_, organism_, format_='markdown', genes_with_summary_only_=F) {
     gene_ranking <- data_frame(symbol=genes_, rank=1:length(genes_))
     symbol2eg <- switch(organism_,
                         mouse = org.Mm.eg.db::org.Mm.egSYMBOL2EG,
@@ -23,13 +23,13 @@ gene_annotation <-
 
     gene_annotation <- data_frame(symbol=character(), summary=character())
     for (record in gene_summary_xml %>% content %>% xml_child %>% xml_children) {
-      gene_symbol <- record %>% xml_find_all('./Name') %>% xml_contents %>% as.character
-      gene_summary <- record %>% xml_find_all('./Summary') %>% xml_contents %>% as.character
-      if (length(gene_summary) > 0) {
-        gene_annotation <- gene_annotation %>% add_row(symbol=gene_symbol, summary=gene_summary)
-      } else if (use_description_) {
-        gene_description <- record %>% xml_find_all('./Description') %>% xml_contents %>% as.character
-        if (length(gene_description) > 0) {
+      gene_symbol      <- record %>% xml_find_all('./Name')        %>% xml_contents %>% as.character
+      gene_description <- record %>% xml_find_all('./Description') %>% xml_contents %>% as.character %>% sprintf('*%s*', .)
+      gene_summary     <- record %>% xml_find_all('./Summary')     %>% xml_contents %>% as.character
+      if (length(gene_description) > 0) {
+        if (length(gene_summary) > 0) {
+          gene_annotation <- gene_annotation %>% add_row(symbol=gene_symbol, summary=paste(gene_description, gene_summary))
+        } else if (!genes_with_summary_only_) {
           gene_annotation <- gene_annotation %>% add_row(symbol=gene_symbol, summary=gene_description)
         }
       }
