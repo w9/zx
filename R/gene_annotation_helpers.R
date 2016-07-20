0926c70d79ce602d00c8eb105bc61a5d434f7a7c
Xun Zhu <zhuxun2@gmail.com>
2016-07-19 17:10:43 -1000

    commit

--- R/gene_annotation_helpers.R
+++ R/gene_annotation_helpers.R
@@ -8,7 +8,7 @@
 #' @import AnnotationDbi
 #' @import dplyr
 gene_annotation <-
-  function(genes_, organism_, format_='markdown', genes_with_summary_only_=F) {
+  function(genes_, organism_, md_filename_=NULL, html_filename_=NULL, verbose_level_=1, show_in_viewer_=T) {
     gene_ranking <- data_frame(symbol=genes_, rank=1:length(genes_))
     symbol2eg <- switch(organism_,
                         mouse = org.Mm.eg.db::org.Mm.egSYMBOL2EG,
@@ -18,32 +18,36 @@ gene_annotation <-
     mapped_genes <- intersect(genes_, mappedkeys(symbol2eg))
     genes_entrez <- symbol2eg[mapped_genes] %>% as.list %>% unlist
 
+    if (verbose_level_ >= 2) message(sprintf('genes_entrez = (length %d)', length(genes_entrez)))
+    if (verbose_level_ >= 2) print(head(genes_entrez, 50))
+
     gene_summary_xml <- paste0(genes_entrez, collapse=',') %>%
       sprintf('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=%s', .) %>% GET
 
-    gene_annotation <- data_frame(symbol=character(), summary=character())
-    for (record in gene_summary_xml %>% content %>% xml_child %>% xml_children) {
-      gene_symbol      <- record %>% xml_find_all('./Name')        %>% xml_contents %>% as.character
-      gene_description <- record %>% xml_find_all('./Description') %>% xml_contents %>% as.character %>% sprintf('*%s*', .)
-      gene_summary     <- record %>% xml_find_all('./Summary')     %>% xml_contents %>% as.character
-      if (length(gene_description) > 0) {
-        if (length(gene_summary) > 0) {
-          gene_annotation <- gene_annotation %>% add_row(symbol=gene_symbol, summary=paste(gene_description, gene_summary))
-        } else if (!genes_with_summary_only_) {
-          gene_annotation <- gene_annotation %>% add_row(symbol=gene_symbol, summary=gene_description)
-        }
-      }
-    }
-    gene_annotation <-  gene_annotation %>% left_join(gene_ranking, by='symbol') %>% select(rank, symbol, summary)
+    if (verbose_level_ >= 3) message('gene_summary_xml = ')
+    if (verbose_level_ >= 3) print(gene_summary_xml)
 
-    if (format_ == 'data_frame') {
-      gene_annotation
-    } else if (format_ == 'markdown') {
-      temp_prefix <- tempfile()
-      md_file <- sprintf('%s.md', temp_prefix)
-      html_file <- sprintf('%s.html', temp_prefix)
-      paste0('# **', gene_annotation$rank, '** ', gene_annotation$symbol, '\n\n', gene_annotation$summary, '\n\n') %>% write(md_file)
-      rmarkdown::render(md_file)
-      rstudioapi::viewer(html_file)
+    gene_annotation <- data_frame(symbol=character(), full_name=character(), summary=character())
+    for (record in gene_summary_xml %>% content %>% xml_find_all('./*/DocumentSummary')) {
+      gene_symbol      <- record %>% xml_find_all('./Name')        %>% xml_contents %>% as.character %>% ifelse(length(.) > 0, ., '')
+      gene_description <- record %>% xml_find_all('./Description') %>% xml_contents %>% as.character %>% ifelse(length(.) > 0, ., '')
+      gene_summary     <- record %>% xml_find_all('./Summary')     %>% xml_contents %>% as.character %>% ifelse(length(.) > 0, ., '')
+
+      gene_annotation <- gene_annotation %>% add_row(symbol=gene_symbol, full_name=gene_description, summary=gene_summary)
     }
+    gene_annotation <-  gene_annotation %>% left_join(gene_ranking, by='symbol') %>% select(rank, symbol, full_name, summary)
+
+    temp_prefix <- tempfile()
+    md_filename   <- ifelse(is.null(md_filename_  ), sprintf('%s.md'  , temp_prefix), md_filename_  )
+    html_filename <- ifelse(is.null(html_filename_), sprintf('%s.html', temp_prefix), html_filename_)
+
+    if (verbose_level_ >= 2) message(sprintf('md_filename = %s', md_filename))
+    if (verbose_level_ >= 2) message(sprintf('html_filename = %s', html_filename))
+
+    with(gene_annotation, paste0('# **', rank, '** ', symbol, '\n\n***', full_name, '***\n\n', summary, '\n\n')) %>% write(md_filename)
+
+    rmarkdown::render(md_filename, output_file=basename(html_filename), output_dir=dirname(html_filename), quiet=ifelse(verbose_level_ >= 3, F, T))
+    if (show_in_viewer_) rstudioapi::viewer(html_filename)
+
+    gene_annotation
   }
