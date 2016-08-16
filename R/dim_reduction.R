3d3c44080cca1d0d68936ae7303ed374189ddf8d
Xun Zhu <zhuxun2@gmail.com>
2016-08-16 11:42:39 -1000

    commit

--- R/dim_reduction.R
+++ R/dim_reduction.R
@@ -27,26 +27,27 @@ dim_reduction <-
 
     if (method == 'pca') {
       pr <- prcomp(x)$x
-      df <- data_frame(pc1=pr[,1], pc2=pr[,2], pc3=pr[,3])
+      df <- data_frame(pr[,1], pr[,2], pr[,3])
     } else if (method == 'pca_scale') {
       non_zeros <- apply(x, 2, function(x)var(x)!=0)
       if (sum(non_zeros) < ncol(x)) message(sprintf('%d of %d features have variance > 0.', sum(non_zeros), ncol(x)))
       pr <- prcomp(x[,non_zeros], scale.=T)$x
-      df <- data_frame(pc_scale1=pr[,1], pc_scale2=pr[,2], pc_scale3=pr[,3])
+      df <- data_frame(pr[,1], pr[,2], pr[,3])
     } else if (method == 'mds_cor') {
       mds <- cmdscale((1-cor(t(x)))^3, k=3)
-      df <- data_frame(mds1=mds[,1], mds2=mds[,2], mds3=mds[,3])
+      df <- data_frame(mds[,1], mds[,2], mds[,3])
     } else if (method == 'tsne') {
       ret <- Rtsne(x, dims=3, perplexity=min(30, floor((nrow(x)-1)/3)))$Y
-      df <- data_frame(tsne1=ret[,1], tsne2=ret[,2], tsne3=ret[,3])
+      df <- data_frame(ret[,1], ret[,2], ret[,3])
     } else if (method == 'tsne_cor') {
       ret <- Rtsne((1-cor(t(x)))^3, dims=3, is_distance=T, perplexity=min(30, floor((nrow(x)-1)/3)))$Y
-      df <- data_frame(tsne_cor1=ret[,1], tsne_cor2=ret[,2], tsne_cor3=ret[,3])
+      df <- data_frame(ret[,1], ret[,2], ret[,3])
     } else if (method == 'tsne_abs_cor') {
       ret <- Rtsne((abs(cor(t(x))))^3, dims=3, is_distance=T, perplexity=min(30, floor((nrow(x)-1)/3)))$Y
-      df <- data_frame(tsne_cor1=ret[,1], tsne_cor2=ret[,2], tsne_cor3=ret[,3])
+      df <- data_frame(ret[,1], ret[,2], ret[,3])
     }
 
+    colnames(df) <- paste0(method, 1:3)
     df_list[[method]] <- df
     coord_list[[method]] <- colnames(df)
   }
